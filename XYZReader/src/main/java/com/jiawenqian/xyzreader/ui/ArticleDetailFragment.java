dda5cf8900037b02fb7c877f6810f64a16722e5f
Jiawen Qian <kevin_qianjiawen@hotmail.com>
2017-01-04 14:42:51 -0500

    revise and palette color

--- XYZReader/src/main/java/com/jiawenqian/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/jiawenqian/xyzreader/ui/ArticleDetailFragment.java
@@ -27,6 +27,11 @@ import android.widget.TextView;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.jiawenqian.xyzreader.R;
 import com.jiawenqian.xyzreader.data.ArticleLoader;
 
@@ -166,6 +171,7 @@ public class ArticleDetailFragment extends Fragment implements
                     .centerCrop()
                     .crossFade()
                     .into(mPhotoView);
+
         } else {
             mRootView.setVisibility(View.GONE);
             titleView.setText("N/A");
--- XYZReader/src/main/java/com/jiawenqian/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/jiawenqian/xyzreader/ui/ArticleListActivity.java
@@ -7,12 +7,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -23,6 +25,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.jiawenqian.xyzreader.R;
 import com.jiawenqian.xyzreader.data.ArticleLoader;
 import com.jiawenqian.xyzreader.data.ItemsContract;
@@ -164,7 +171,7 @@ import butterknife.ButterKnife;
             }
 
             @Override
-            public void onBindViewHolder(ViewHolder holder, int position) {
+            public void onBindViewHolder(final ViewHolder holder, int position) {
                 mCursor.moveToPosition(position);
 
                 holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
@@ -176,12 +183,39 @@ import butterknife.ButterKnife;
                                 + " by "
                                 + mCursor.getString(ArticleLoader.Query.AUTHOR));
 
-                Glide.with(getApplicationContext())
+                /*Glide.with(getApplicationContext())
                         .load(mCursor.getString(ArticleLoader.Query.THUMB_URL))
                         .centerCrop()
                         .crossFade()
                         .into(holder.thumbnailView);
+                holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));*/
+
+                //holder.authorView.setText(cursor.getString(ArticleLoader.Query.AUTHOR));
                 holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+                Glide.clear(holder.thumbnailView);
+                Glide.with(holder.thumbnailView.getContext())
+                        .load(mCursor.getString(ArticleLoader.Query.THUMB_URL))
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .dontAnimate()
+                        .listener(new RequestListener<String, GlideDrawable>() {
+                            @Override
+                            public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(GlideDrawable resource, String model,
+                                                           Target<GlideDrawable> target,
+                                                           boolean isFromMemoryCache, boolean isFirstResource) {
+                                Bitmap bitmap = ((GlideBitmapDrawable) resource.getCurrent()).getBitmap();
+                                Palette palette = Palette.generate(bitmap);
+                                int defaultColor = 0xFF333333;
+                                int color = palette.getDarkMutedColor(defaultColor);
+                                holder.itemView.setBackgroundColor(color);
+                                return false;
+                            }
+                        })
+                        .into(holder.thumbnailView);
             }
 
             @Override
